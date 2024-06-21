b269afc04b765985865ab4b954bffb6bc894ca66
intansemc2 <intansemc2@gmail.com>
2024-06-21 09:31:58 +0700

    Change to svelte SPA

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="w-full h-screen flex items-center justify-center fixed">
+	<div class="hero min-h-screen bg-base-200">
+		<div class="hero-content text-center">
+			<div class="max-w-md">
+				<h1 class="text-9xl font-bold">{$page.status}</h1>
+				<div>{$page.error?.message}</div>
+			</div>
+		</div>
+	</div>
+</div>
